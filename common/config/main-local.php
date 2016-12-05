7b958cbdb2ab29822e9a45c9ca080e0383a66054
vikomanurung <vicozwar@gmail.com>
2016-12-05 13:05:46 +0700

    add file main-local.php

---
+++ common/config/main-local.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+//          'dsn' => 'mysql:host=ap-cdbr-azure-east-c.cloudapp.net;dbname=batak',
+            'dsn' => 'mysql:host=localhost;dbname=findtutor',
+            'username' => 'root',
+            'password' => '',
+//                        'username' => 'ba18dc2bb1a798',
+//            'password' => 'd4693502',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
